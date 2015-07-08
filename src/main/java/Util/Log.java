d13fa5754ecbaa8c0306405c1d63c2dcd69543a8
rubytobi <tobias-ruby@t-online.de>
2015-07-08 09:09:05 +0200

    Log erweitert
    nun auch #info und #debug Meldungen möglich

--- src/main/java/Util/Log.java
+++ src/main/java/Util/Log.java
@@ -2,11 +2,19 @@ package Util;
 
 public class Log {
 	public static void i(String className, String s) {
-		System.out.println("#debug\t" + className + "\t" + s);
+		System.out.println("#info\t" + className + "\t" + s);
 	}
 
 	public static void i(String s) {
 		i("-", s);
+	}
+
+	public static void d(String className, String s) {
+		System.out.println("#debug\t" + className + "\t" + s);
+	}
+
+	public static void d(String s) {
+		d("-", s);
 	}
 
 	public static void e(String className, String s) {
