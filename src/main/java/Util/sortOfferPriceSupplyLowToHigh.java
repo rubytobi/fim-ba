a0cd2d98fa5db8afe1a4464e4a075a746ea0ca00
hendleje <jenny2912@gmx.de>
2015-07-27 12:22:56 +0200

    Marketplace, Fridge
    Marketplace sortiert Angebote nach Preis
    Fridge gibt negatives Lasprofil aus

---
+++ src/main/java/Util/sortOfferPriceSupplyLowToHigh.java
@@ -0,0 +1,23 @@
+package Util;
+
+import java.util.Comparator;
+
+import Entity.Offer;
+
+
+public class sortOfferPriceSupplyLowToHigh implements Comparator<Offer> {
+	@Override
+	public int compare(Offer offer1, Offer offer2) {
+		double price1 = offer1.getPrice();
+		double price2 = offer2.getPrice();
+		if (price1<price2) {
+			return -1;
+		}
+		else if (price1 == price2) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+}
